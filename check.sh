d73719853489344e6dcda28254238384536bd632
Costel Radulescu <wizard.ro@gmail.com>
2016-01-26 00:56:02 +0200

    added postgresql and pgadmin3

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tools=( "zsh" "xflux" "vim" "unzip" "curl" "java" "mvn" "python" "g++" "virtualbox" "vagrant" );
+tools=( "zsh" "xflux" "vim" "unzip" "curl" "java" "mvn" "python" "g++" "virtualbox" "vagrant" "posgresql" "pgadmin3" );
 for tool in ${tools[@]}; 
 do
  echo "=======";

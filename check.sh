7417c073da1a305f05fd570430b4ec04efe3f5db
Costel Radulescu <wizard.ro@gmail.com>
2016-04-08 22:51:53 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tools=( "zsh" "xflux" "vim" "unzip" "curl" "java" "mvn" "python" "g++" "virtualbox" "vagrant" "psql" "pgadmin3" );
+tools=( "tmux" "tree" "zsh" "xflux" "vim" "unzip" "curl" "java" "mvn" "python" "g++" "virtualbox" "vagrant" "psql" "pgadmin3" );
 for tool in ${tools[@]}; 
 do
  echo "=======";
